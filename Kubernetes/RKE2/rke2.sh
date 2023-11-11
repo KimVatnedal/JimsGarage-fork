ac1ec2feb3c85a0f0b62469f8dd965e934c561be
KimVatnedal <57816414+KimVatnedal@users.noreply.github.com>
2023-11-11 18:07:23 +0100

    test

--- Kubernetes/RKE2/rke2.sh
+++ Kubernetes/RKE2/rke2.sh
@@ -24,21 +24,21 @@ echo -e " \033[32;5m                                                           \
 KVVERSION="v0.6.3"
 
 # Set the IP addresses of the admin, masters, and workers nodes
-admin=192.168.3.5
-master1=192.168.3.21
-master2=192.168.3.22
-master3=192.168.3.23
-worker1=192.168.3.24
-worker2=192.168.3.25
+admin=192.168.20.36
+master1=192.168.20.37
+master2=192.168.20.38
+master3=192.168.20.39
+worker1=192.168.20.40
+worker2=192.168.20.41
 
 # User of remote machines
-user=ubuntu
+user=kim
 
 # Interface used on remotes
 interface=eth0
 
 # Set the virtual IP address (VIP)
-vip=192.168.3.50
+vip=192.168.20.19
 
 # Array of all master nodes
 allmasters=($master1 $master2 $master3)
@@ -56,10 +56,10 @@ all=($master1 $master2 $master3 $worker1 $worker2)
 allnomaster1=($master2 $master3 $worker1 $worker2)
 
 #Loadbalancer IP range
-lbrange=192.168.3.60-192.168.3.80
+lbrange=192.168.20.260-192.168.20.280
 
 #ssh certificate name variable
-certName=id_rsa
+certName=id_ed25519_bazzite_desktop
 
 #############################################
 #            DO NOT EDIT BELOW              #
@@ -95,7 +95,7 @@ done
 # create RKE2's self-installing manifest dir
 sudo mkdir -p /var/lib/rancher/rke2/server/manifests
 # Install the kube-vip deployment into rke2's self-installing manifest folder
-curl -sL https://raw.githubusercontent.com/JamesTurland/JimsGarage/main/Kubernetes/RKE2/k3s |  vipAddress=$vip vipInterface=$interface sh | sudo tee /var/lib/rancher/rke2/server/manifests/kube-vip.yaml
+curl -sL https://raw.githubusercontent.com/KimVatnedal/JimsGarage-fork/rke2-cilium/Kubernetes/RKE2/k3s |  vipAddress=$vip vipInterface=$interface sh | sudo tee /var/lib/rancher/rke2/server/manifests/kube-vip.yaml
 # Find/Replace all k3s entries to represent rke2
 sudo sed -i 's/k3s/rke2/g' /var/lib/rancher/rke2/server/manifests/kube-vip.yaml
 # copy kube-vip.yaml to home directory
